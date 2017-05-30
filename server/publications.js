0b2256baed7b6d9c4e8af3977ebc8995e9db5e5f
kawamuraruiz <tobyoldman.mustard@gmail.com>
2017-05-30 10:45:20 -0600

    se creo un mecanismo de publicación (¬'-')¬

--- server/publications.js
+++ server/publications.js
@@ -5,3 +5,8 @@ Meteor.publish('posts', function() {
 Meteor.publish('comments', function() {  
     return Comments.find();
 });
+
+Meteor.publish('comments', function(postId) {  
+ check(postId, String);  
+ return Comments.find({postId: postId});
+});
